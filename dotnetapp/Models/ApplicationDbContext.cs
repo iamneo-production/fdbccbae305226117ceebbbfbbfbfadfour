740fbb9dff31580e8c6ce55f243cb34b667a25da
Iamneo <techadmin@iamneo.ai>
2023-09-26 09:34:35 +0000

    Backup Commit

--- dotnetapp/Models/ApplicationDbContext.cs
+++ dotnetapp/Models/ApplicationDbContext.cs
@@ -1,8 +1,10 @@
 using dotnetapp.Models;
 using System;
- 
+using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+
+
 
 public class ApplicationDbContext : DbContext
     {
@@ -14,6 +16,14 @@ public ApplicationDbContext()
         {
         }
 
-        
+        public virtual DbSet<Booking> Batches { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("User ID=sa;password=examlyMssql@123;server=localhost;Database=FrenchTutionDB;trusted_connection=false;Persist Security Info=False;Encrypt=False;");
+            }
+        }
         // Write your ApplicationDbContext here...
     }
